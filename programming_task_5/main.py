1652ad7f4f332f5d0a7bb32901aed7c2858728da
Charbel-PMI-25 <71026562+Charbel-PMI-25@users.noreply.github.com>
2020-11-14 08:04:35 +0200

    Update main.py

--- programming_task_5/main.py
+++ programming_task_5/main.py
@@ -10,9 +10,8 @@ while True:
     1.) Search orders with value
     2.) Delete orders by ID
     3.) Add order
-    4.) Edit order by ID
-    5.) Print all orders
-    6.)Exit''' + '\n')
+    4.) Print all orders
+    5.)Exit''' + '\n')
     if userchoice == '1':
         val = input('Input value for order: ')
         ls.search_in_list(val)
@@ -24,15 +23,11 @@ while True:
         new_order = input('Input info for new order: ')
         ls.add(new_order, file_name)
     elif userchoice == '4':
-        id = input('Input id: ')
-        new_order = input('Input info for new order: ')
-        ls.edit(new_order, file_name, id)
-    elif userchoice == '5':
         if type(ls) == 'NoneType':
             print('Your list_orders does not have any orders!')
         else:
             ls.print_list()
-    elif userchoice == '6':
+    elif userchoice == '5':
         print("thank fot using us !!!")
         break
     else:
